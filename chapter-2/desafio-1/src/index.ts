8390a9c286df674bc977943b520fc0af41acbd0f
Renan Modenese <renan.modenese@gmail.com>
2022-06-09 16:02:14 -0300

    Incluindo documentação (desafio #2)

--- chapter-2/desafio-1/src/index.ts
+++ chapter-2/desafio-1/src/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
-
+import swaggerUi from 'swagger-ui-express';
+import swaggerFile from './swagger.json';
 import { usersRoutes } from "./routes/users.routes";
 
 const app = express();
+
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(express.json());
 
